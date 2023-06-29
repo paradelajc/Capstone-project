3641a6c1a32f02429fda1c2bf51241db526b5303
paradelajc <jcp1006@gmail.com>
2023-06-29 10:39:46 -0700

    made corrections to models.py

--- restaurant/models.py
+++ restaurant/models.py
@@ -7,10 +7,13 @@ class Menu(models.Model):
   price = models.DecimalField(max_digits=10, decimal_places=2)
   inventory = models.IntegerField()
 
-  def __srt__(self):
+  def __str__(self):
     return self.title
 
 class Booking(models.Model):
   name = models.CharField(max_length=255)
   no_of_guests = models.IntegerField()
   booking_date = models.DateTimeField()
+
+  def __str__(self):
+    return self.name
