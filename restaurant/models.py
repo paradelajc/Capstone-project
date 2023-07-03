1a8b70dd4ae17af822c685926fd72595790d3acd
paradelajc <jcp1006@gmail.com>
2023-07-02 22:15:45 -0700

    Added test cases

--- restaurant/models.py
+++ restaurant/models.py
@@ -8,6 +8,9 @@ class MenuItem(models.Model):
   price = models.DecimalField(max_digits=10, decimal_places=2)
   inventory = models.IntegerField()
 
+  def get_item(self):
+    return f'{self.title}: {str(self.price)}'
+
   def __str__(self):
     return f'{self.title}: {str(self.price)}'
 
